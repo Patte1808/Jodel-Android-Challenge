9bcff10b05ffe2bccab55e582bc4b4350b0f03b3
Pattelicious <vosspatr@informatik.hu-berlin.de>
2017-11-16 21:57:19 +0100

    Add test skeletong

---
+++ app/src/test/java/com/jodelapp/features/albums/AlbumListAdapterTest.java
@@ -0,0 +1,36 @@
+package com.jodelapp.features.albums;
+
+import android.support.v7.widget.RecyclerView;
+
+import com.jodelapp.features.albums.models.AlbumPresentationModel;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Pattelicious on 16.11.17.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class AlbumListAdapterTest {
+
+    RecyclerView recyclerView = Mockito.mock(RecyclerView.class);
+
+    void shouldOpenPhotoView_onClickItem() {
+        List<AlbumPresentationModel> items = new ArrayList<>();
+        AlbumPresentationModel albumPresentationModel = Mockito.mock(AlbumPresentationModel.class);
+        items.add(albumPresentationModel);
+
+        /*
+            Todo:
+            Can't unit test this, because I can't invoke the constructor
+            How to handle that?
+         */
+        //AlbumListAdapter adapter = Mockito.when(AlbumListAdapter.class)
+
+    }
+}
